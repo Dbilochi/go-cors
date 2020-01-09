55df7ae8cba0ee02838cb256b61e13d063f9f490
Deeksha Bilochi <deekshabilochi@Deekshas-MacBook-Pro.local>
2020-01-09 16:18:13 +0530

    enable cors

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"github.com/rs/cors"
 	"github.com/gorilla/mux"
 )
 
@@ -16,15 +17,20 @@ func FetchBooks(w http.ResponseWriter, r *http.Request) {
 	books = append(books, "When Breathe becomes air")
 	books = append(books, "The Magic")
 	books = append(books, "The Secret")
-
-
 	response, _ := json.Marshal(books)
 	w.Write(response)
 }
 
 func main() {
 	r := mux.NewRouter()
+	handler := cors.Default().Handler(r)
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "HEAD", "POST","PUT", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "content-type", "X-Requested-With", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "Screen"},
+	})
+	handler = c.Handler(handler)
 	r.HandleFunc("/getbooks", FetchBooks).Methods("GET")
 	log.Printf("Server Started");
-	log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), r))
+	log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), handler))
 }
